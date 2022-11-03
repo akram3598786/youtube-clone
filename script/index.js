a30058e04a8f5a6bf8961f0a223334c76c58ba41
akram3598786 <akramali03598786@gmail.com>
2022-11-03 16:28:42 +0530

    added responsiveness

--- script/index.js
+++ script/index.js
@@ -23,9 +23,7 @@ function showData() {
         title.innerText = video.snippet.title;
         chanel.innerText = "Channel : " + video.snippet.channelTitle;
         card.append(image, title, chanel);
-
         box.append(card);
-
         card.addEventListener("click", function () {
             let videoId = video.id.videoId;
             localStorage.setItem("videoId", JSON.stringify(videoId))
@@ -38,7 +36,7 @@ function showData() {
 async function searchData() {
     event.preventDefault();
     let query = document.getElementById("input").value;
-    console.log(query);
+   // console.log(query);
     let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?maxResults=20&q=${query}&type=video&regionCode=IN&part=snippet&key=AIzaSyA5q3pQA9OmPNeZUgXfjTgirhyqdzV4I5M`);
     let data = await res.json();
     data = data.items;
